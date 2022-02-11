9224976e4cbc0be2770d73eb088a780b6ed6fd1d
Hayden-J <1761373255@qq.com>
2022-02-11 20:13:04 +0800

    complete infinite scroll

---
+++ MiniLiveBroadcastApp/Utils.swift
@@ -0,0 +1,20 @@
+//
+//  Utils.swift
+//  MiniLiveBroadcastApp
+//
+//  Created by 夏侯臻 on 2022/2/11.
+//
+
+import Foundation
+import UIKit
+
+
+extension Double {
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+}
+
+extension CGFloat {
+    static let screenWidth = UIScreen.main.bounds.width
+    static let screenHeight = UIScreen.main.bounds.height
+}
--- MiniLiveBroadcastApp/ViewController.swift
+++ MiniLiveBroadcastApp/ViewController.swift
@@ -9,11 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let scrollView = ScrollPageView(frame: CGRect(x: 0, y: 0, width: .screenWidth, height: .screenHeight))
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+
+
+        view.addSubview(scrollView)
+
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: view.topAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            scrollView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            scrollView.rightAnchor.constraint(equalTo: view.rightAnchor)
+        ])
     }
-
-
 }
-
